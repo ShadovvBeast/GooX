c9991b74844fc56081305171bbb3db9d70deb51d
Asaf(ShadowBeast) <ShadovvBeast@gmail.com>
2015-02-07 13:57:00 +0200

    GooX - Initial commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'username' => 'root',
+    'password' => '60636063',
+    'charset' => 'utf8',
+];
